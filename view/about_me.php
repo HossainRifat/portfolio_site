91924b7a38181f1847a12fec90a51b0177557eeb
HossainRifat <rh140025@gmail.com>
2022-05-12 23:38:51 +0600

    about static done

--- view/about_me.php
+++ view/about_me.php
@@ -13,10 +13,20 @@
         <table>
             <tr>
                 <td>
-                    <img class="profile-image" src="../photo/profile.jpg" alt="profile" height="300" width="300">
+                    <img class="profile-image" src="../photo/profile_2.jpg" alt="profile" height="280" width="280">
+                </td>
+                <td>
+                    <p class="name">Md. Rifat Hossain</p>
+                    <p class="skill">Programer, Application Developer, Web Developer</p>
+                    <p class="details">Form Singair, Manikgon distrck. Studying Computer Science and Engineering at American International University-Bangladesh. Good knowledge in Programming languages like C#, Java, Python, C++, PHP, JS. I am working on some desktop applications and websites. Its my last year of University and i am researching on applications of blockchain. My goals are not fixed yet!!!</p>
+                    <p class="button-box"><a href="" class="button">Download CV</a><a href="" class="button">Contact</a></p>
                 </td>
             </tr>
         </table>
+    </div>
+
+    <div class="note">
+        <p>**This site is under development**</p>
     </div>
 </body>
 </html>
